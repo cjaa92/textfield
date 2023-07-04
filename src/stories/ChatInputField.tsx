a2a45813be275e62f72776108e09904a853429fa
Carlos Jesús Almeraz Aizpuru <carlos@almeraz.org>
2023-07-04 02:25:19 -0600

    Ajustes de Styles

--- src/stories/ChatInputField.tsx
+++ src/stories/ChatInputField.tsx
@@ -40,17 +40,32 @@ export const ChatInputFields = ({
   const showWhenAssigned = assigned ? "": " hide";
   const grayoutText = assigned ? "replyAs" : ""
   return (
-    <div className='chat-input-field' {...props} onClick={() => updateArgs({ isOpen: !isOpen })}>
-      <label className={['replyAs',showWhenAssigned].join(' ')}>Replying as&nbsp;</label>
-      <div className={['extraComps', grayoutText].join(' ')}>
-        <b><FiUserPlus className={['add-icon', showWhenUnAssigned].join(' ')}  /></b> 
-        <Image src={image} alt="" className={['img-profile', showWhenAssigned].join(' ')} width={15} height={15}/>
+    <div className='chat-input-field' {...props} >
+      <label className={['replyAs',showWhenAssigned].join(' ')}>Replying as</label>
+      <button className={['extraComps', grayoutText].join(' ')} >
+        <FiUserPlus  className={['add-icon', showWhenUnAssigned].join(' ')} />
+        <Image src={image} alt="" className={['img-profile', showWhenAssigned].join(' ')} width={20} height={20}/>
+        &nbsp;&nbsp;
         {name2Show}
-      </div>
+      </button>
       <input className={['text-input', showWhenAssigned].join(' ')} type='text' />
-      <div className='extraComps send'>
-        <FiSend className='send-icon'/> 
-      </div>
+      <button className='sendComps' >
+        <FiSend  className='send-icon' />
+      </button>
     </div>
+
+
+    // <div className='chat-input-field' {...props} >
+    //   <label className={['replyAs',showWhenAssigned].join(' ')}>Replying as&nbsp;</label>
+    //   <div className={['extraComps', grayoutText].join(' ')}>
+    //     <FiUserPlus color='#6B42EE' size="24" strokeWidth="2.5" /> 
+    //     <Image src={image} alt="" className={['img-profile', showWhenAssigned].join(' ')} width={15} height={15}/>
+    //     {name2Show}
+    //   </div>
+    //   <input className={['text-input', showWhenAssigned].join(' ')} type='text' />
+    //   <div className='extraComps send'>
+    //     <FiSend className='send-icon'/> 
+    //   </div>
+    // </div>
   );
 };
